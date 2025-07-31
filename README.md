530d8a29b7876185a342ce566caa7de1a2863082
kiwi-706 <223867857+kiwi-706@users.noreply.github.com>
2025-07-31 18:42:58 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Aliucord Plugins collection
+Plugins for [Aliucord](https://github.com/Aliucord)
+
+## Download
+Download from [here](https://github.com/kiwi-706/AliucordPlugins/tree/builds)
+
+## [Nitrospoof](/plugins/NitroSpoof)
+Use nitro emotes without nitro. (Originally developed by [Xinto](https://github.com/X1nto))
---
+++ plugins/NitroSpoof/build.gradle.kts
@@ -0,0 +1,14 @@
+description = "Use all emotes in any server without Nitro."
+version = "1.0.11"
+
+aliucord.changelog.set("""
+  # 1.0.11
+  * Make spoofed emojis display as normal emoji
+  * Use markdown hyperlinks instead of raw emoji URLs
+  * The troll is gone - saving over 4 kB of your precious storage
+  * Note: Originally developed by Xinto
+""".trimIndent())
+
+aliucord {
+  author("Xinto", 423915768191647755L)
+}
